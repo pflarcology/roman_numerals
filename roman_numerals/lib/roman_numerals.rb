cfad67fc67ff5230483dd2348ce769d10c0865ce
pflarcology <rhrg500@gmail.com>
2016-11-15 09:49:13 +0000

    created numbers hash

--- roman_numerals/lib/roman_numerals.rb
+++ roman_numerals/lib/roman_numerals.rb
@@ -1,6 +1,11 @@
 class Roman
 
-def roman_numerals(number)
-end
+  def roman_numerals(number)
+    number == 1? "I" : "II"
+  end
+
+  def numbers_hash
+
+  end
 
 end
--- roman_numerals/spec/roman_numerals_spec.rb
+++ roman_numerals/spec/roman_numerals_spec.rb
@@ -2,5 +2,26 @@ require 'roman_numerals'
 
   describe Roman do
 
-    it {is_expected.to respond_to(:roman_numerals).with(1).argument }
+    context 'numerals test' do
+
+      it 'should return "I" when 1 is inputed' do
+        expect(subject.roman_numerals(1)).to eq "I"
+      end
+
+      it 'should return "II" when 2 is inputed' do
+        expect(subject.roman_numerals(2)).to eq "II"
+      end
+      # 
+      # it 'should return "III" when 3 is inputed' do
+      #   expect(subject.roman_numerals(3)).to eq "III"
+      # end
+
+    end
+
+    context 'hash tests' do
+
+      it { is_expected.to respond_to(:numbers_hash)}
+
+    end
+
   end
